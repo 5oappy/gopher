27da8777d8e46e27d2f5482f2e9d8c6d745b7b1c
Soap <u7108792@anu.edu.au>
2024-04-19 02:13:06 +1000

    added client template as well as begun simple terminal ui implementation

---
+++ code/client.py
@@ -0,0 +1,46 @@
+import socket
+import time
+
+class GopherClient:
+    def __init__(self, server_host, server_port):
+        self.server_host = server_host
+        self.server_port = server_port
+        self.socket = None
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.server_host, self.server_port))
+    
+    def close(self):
+        if self.socket:
+            self.socket.close()
+    
+    def send_request(self, request):
+        if self.socket:
+            self.socket.sendall(request.encode())
+
+    def receive_response(self):
+        if self.socket:
+            return self.socket.recv(4096).decode()
+
+    def scan_directory(self, directory):
+        # Implement directory scanning logic here
+        
+        pass
+
+    def download_file(self, file_path):
+        # Implement file download logic here
+        pass
+
+    def run(self):
+        # Implement main logic to connect, scan directories, download files, and print statistics
+        pass
+
+if __name__ == "__main__":
+    # Set server host and port
+    server_host = "your_server_host"
+    server_port = 70  # Gopher server port
+    
+    # Create and run the client
+    client = GopherClient(server_host, server_port)
+    client.run()
